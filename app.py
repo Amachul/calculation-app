f4283bda4e3fc5a7c987c49e0d470f9c755b540f
AnastasiiaMac <for_nastia@ukr.net>
2024-03-08 16:21:47 +0100

    multiplication updated

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ elif s.find("-")>0:
     a = int(s[:s.find("-")].strip())
     b = int(s[s.find("-")+1:].strip())
     print(f'Result is {a-b}')
-elif s.find("") > 0:
-    a = int(s[:s.find("")].strip())
+elif s.find("*") > 0:
+    a = int(s[:s.find("*")].strip())
     b = int(s[s.find("*") + 1:].strip())
     print(f'Result is {a * b}')
 else:
